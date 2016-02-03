a45a9b8b399b491c67fba99363c914cf89faab71
Simon Coudeville <simon.coudeville@wijs.be>
2016-02-03 22:56:33 +0100

    carousel update + design updates

--- _site/js/theme.concat.js
+++ _site/js/theme.concat.js
@@ -49,124 +49,71 @@ if (Modernizr.history) {
 
 var chopstick =
 {
-    // init, something like a constructor
-    init: function()
-    {
-        // chopstick.loadObject(chopstick.mobileNav, 'chopstick.mobileNav');
-        // chopstick.loadObject(chopstick.headerScroll, 'chopstick.headerScroll');
-        // chopstick.loadObject(chopstick.ready, 'chopstick.ready');
+        // init, something like a constructor
+        init: function()
+        {
+                // chopstick.loadObject(chopstick.mobileNav, 'chopstick.mobileNav');
+                // chopstick.loadObject(chopstick.headerScroll, 'chopstick.headerScroll');
+                // chopstick.loadObject(chopstick.ready, 'chopstick.ready');
 
-        // chopstick.mobileNav;
-        // chopstick.headerScroll;
-        // chopstick.ready;
+                // chopstick.mobileNav;
+                // chopstick.headerScroll;
+                // chopstick.ready;
 
-        headerScroll();
+                headerScroll();
 
-        var flky = new Flickity( '.js-gallery', {
-  // options, defaults listed
+                var $gallery = $('.js-gallery').flickity({
+                        accessibility: true,
+                        autoPlay: false,
+                        cellAlign: 'left',
+                        cellSelector: undefined,
+                        contain: true,
+                        draggable: true,
+                        freeScroll: false,
+                        selectedAttraction: 0.2,
+                        friction: 0.8,
+                        initialIndex: 0,
+                        lazyLoad: true,
+                        percentPosition: true,
+                        prevNextButtons: false,
+                        pageDots: false,
+                        resize: true,
+                        rightToLeft: false,
+                        setGallerySize: false,
+                        watchCSS: false,
+                        wrapAround: false
+                });
 
-  accessibility: true,
-  // enable keyboard navigation, pressing left & right keys
+                // var flky = new Flickity( '.js-gallery', {
+                //         accessibility: true,
+                //         autoPlay: false,
+                //         cellAlign: 'left',
+                //         cellSelector: undefined,
+                //         contain: true,
+                //         draggable: true,
+                //         freeScroll: false,
+                //         selectedAttraction: 0.2,
+                //         friction: 0.8,
+                //         initialIndex: 0,
+                //         lazyLoad: true,
+                //         percentPosition: true,
+                //         prevNextButtons: false,
+                //         pageDots: false,
+                //         resize: true,
+                //         rightToLeft: false,
+                //         setGallerySize: false,
+                //         watchCSS: false,
+                //         wrapAround: false
+                // });
 
-  autoPlay: false,
-  // advances to the next cell
-  // if true, default is 3 seconds
-  // or set time between advances in milliseconds
-  // i.e. `autoPlay: 1000` will advance every 1 second
+                $gallery.on( 'staticClick', function( event, pointer, cellElement, cellIndex ) {
+                        if ( typeof cellIndex == 'number' ) {
+                                $gallery.flickity( 'select', cellIndex );
+                        }
+                });
 
-  cellAlign: 'left',
-  // alignment of cells, 'center', 'left', or 'right'
-  // or a decimal 0-1, 0 is beginning (left) of container, 1 is end (right)
-
-  cellSelector: undefined,
-  // specify selector for cell elements
-
-  contain: true,
-  // will contain cells to container
-  // so no excess scroll at beginning or end
-  // has no effect if wrapAround is enabled
-
-  draggable: true,
-  // enables dragging & flicking
-
-  freeScroll: false,
-  // enables content to be freely scrolled and flicked
-  // without aligning cells
-
-  selectedAttraction: 0.2,
-
-  friction: 0.8,  // smaller number = easier to flick farther
-
-  initialIndex: 0,
-  // zero-based index of the initial selected cell
-
-  lazyLoad: true,
-  // enable lazy-loading images
-  // set img data-flickity-lazyload="src.jpg"
-  // set to number to load images adjacent cells
-
-  percentPosition: true,
-  // sets positioning in percent values, rather than pixels
-  // Enable if items have percent widths
-  // Disable if items have pixel widths, like images
-
-  prevNextButtons: false,
-  // creates and enables buttons to click to previous & next cells
-
-  pageDots: false,
-  // create and enable page dots
-
-  resize: true,
-  // listens to window resize events to adjust size & positions
-
-  rightToLeft: false,
-  // enables right-to-left layout
-
-  setGallerySize: true,
-  // sets the height of gallery
-  // disable if gallery already has height set with CSS
-
-  watchCSS: false,
-  // watches the content of :after of the element
-  // activates if #element:after { content: 'flickity' }
-  // IE8 and Android 2.3 do not support watching :after
-  // set watch: 'fallbackOn' to enable for these browsers
-
-  wrapAround: false
-  // at end of cells, wraps-around to first for infinite scrolling
-
-});
-
-        console.log("javascript is locked and loaded!") // for testing purposes. Check your console. Delete after you finished reading this. :-)
-    },
-
-    /**
-     * This function will load an object by a given name
-     *
-     * If the object doesn't exist no error will be thrown
-     * But if object exists but doesn't have an init method it will throw an error
-     *
-     * If everything is ok we'll initiate the given object
-     */
-    loadObject: function(obj, name)
-    {
-        // create object based on a name
-        // var objName = window[objName];
-
-        // check if object exists
-        if(typeof obj != 'undefined') {
-
-            // check if object has a method init
-            if (typeof obj.init == 'undefined') {
-                // we will throw an error so the designer / developer know there's a problem
-                throw new Error('ERROR: "' + name + '" does not have an init function');
-
-            } else {
-                // everything is fine so initiate
-                obj.init();
-            }
-        }
-    }
+                console.log("javascript is locked and loaded!") // for testing purposes. Check your console. Delete after you finished reading this. :-)
+        },
 };
 
 var headerScroll = function()
