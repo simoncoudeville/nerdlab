25784b5f40d527257241b839d450880219bdf79d
Simon Coudeville <simon.coudeville@wijs.be>
2016-05-07 16:11:59 +0200

    design changes

--- _site/js/theme.concat.js
+++ _site/js/theme.concat.js
@@ -118,16 +118,23 @@ var chopstick =
 
 var headerScroll = function()
 {
-    var $header = $('.js-header');
+    var header = $('.js-header');
+    var body = $('body');
+    var headerOffset = $('.js-header').offset().top;
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= 70) {
-            $header.addClass("shrink");
-        } else {
-            $header.removeClass("shrink");
-        }
+
+        // if (scroll >= 1) {
+        //     body.addClass("shrink");
+        // } else {
+        //     header.removeClass("shrink");
+        // }
+
+        if (scroll >= headerOffset) body.addClass('has-fixed-header');
+        else body.removeClass('has-fixed-header');
+
     });
 };
 
--- js/_components/_headerscroll.js
+++ js/_components/_headerscroll.js
@@ -1,14 +1,21 @@
 var headerScroll = function()
 {
-    var $header = $('.js-header');
+    var header = $('.js-header');
+    var body = $('body');
+    var headerOffset = $('.js-header').offset().top;
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= 70) {
-            $header.addClass("shrink");
-        } else {
-            $header.removeClass("shrink");
-        }
+
+        // if (scroll >= 1) {
+        //     body.addClass("shrink");
+        // } else {
+        //     header.removeClass("shrink");
+        // }
+
+        if (scroll >= headerOffset) body.addClass('has-fixed-header');
+        else body.removeClass('has-fixed-header');
+
     });
 };
