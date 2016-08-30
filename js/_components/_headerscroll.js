c03ee2f9e36793b0e33a5c7e112d97a87b695b48
Simon Coudeville <simon.coudeville@wijs.be>
2016-08-30 22:25:29 +0200

    still trying to fix links

--- js/_components/_headerscroll.js
+++ js/_components/_headerscroll.js
@@ -2,17 +2,14 @@ var headerScroll = function()
 {
     var header = $('.js-header');
     var headerOffset = $('.js-header').outerHeight();
-    console.log(headerOffset);
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
         if (scroll >= headerOffset/3) {
             header.addClass("is-shrinken");
-            console.log(scroll);
         } else {
             header.removeClass("is-shrinken");
-            console.log(scroll);
         }
     });
 };
