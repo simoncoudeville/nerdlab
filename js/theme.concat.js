a92db8d32569f9765dd86bd0ec1e36f6db7e9cfb
Simon Coudeville <simon.coudeville@wijs.be>
2016-09-27 22:39:34 +0200

    added animation to mobilenav

--- js/theme.concat.js
+++ js/theme.concat.js
@@ -88,11 +88,20 @@ var headerScroll = function()
 var mobileNav = function()
 {
     var header = $('.js-header');
+    var nav = $('.js-nav');
     var trigger = $('.js-nav-trigger');
+    var triggerUp = $('.js-nav-trigger-up');
 
     trigger.on('click', function() {
         $(this).toggleClass('is-active');
-        header.toggleClass('has-visible-nav');
+        nav.slideToggle('fast');
+        // header.toggleClass('has-visible-nav');
+    });
+
+    triggerUp.on('click', function() {
+        trigger.removeClass('is-active');
+        nav.slideUp('fast');
+        // header.toggleClass('has-visible-nav');
     });
 };
 
