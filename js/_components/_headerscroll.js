c16531196cf32ddc3298ffe29c0fed66a1603812
Simon Coudeville <simon.coudeville@wijs.be>
2016-11-22 22:04:48 +0100

    added scroll to fixed plugin

--- js/_components/_headerscroll.js
+++ js/_components/_headerscroll.js
@@ -3,13 +3,15 @@ var headerScroll = function()
     var header = $('.js-header');
     var headerOffset = $('.js-header').outerHeight();
 
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+    header.scrollToFixed();
 
-        if (scroll >= headerOffset/3) {
-            header.addClass("is-shrinken");
-        } else {
-            header.removeClass("is-shrinken");
-        }
-    });
+    // $(window).scroll(function() {
+    //     var scroll = $(window).scrollTop();
+    //
+    //     if (scroll >= headerOffset) {
+    //         header.addClass("is-shrinken");
+    //     } else {
+    //         header.removeClass("is-shrinken");
+    //     }
+    // });
 };
